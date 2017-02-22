df5a254a01b75923a68a23fb79ef86d86821b5e4
wutentcoding <wutengwuteng@outlook.com>
2017-02-22 16:54:58 +0800

    adding commit

--- state_machine/nameset/namedb.py
+++ state_machine/nameset/namedb.py
@@ -1,4 +1,5 @@
 import shutil
+import traceback
 
 import indexer
 from db import *
@@ -78,3 +79,88 @@ class ZonefileManageDB(indexer.StateEngine):
 
     def get_db_path(self):
         return self.db_filename
+
+    def commit_operation(self, nameop, current_block_number):
+        """
+        Commit an operation, thereby carrying out a state transition.
+        """
+
+        # have to have read-write disposition
+        if self.disposition != DISPOSITION_RW:
+            log.error("FATAL: borrowing violation: not a read-write connection")
+            traceback.print_stack()
+            os.abort()
+
+        cur = self.db.cursor()
+        op_seq = None
+        op_seq_type_str = None
+        opcode = nameop.get('opcode', None)
+        history_id = None
+
+        try:
+            assert opcode is not None, "Undefined op '%s'" % nameop['op']
+        except Exception, e:
+            log.exception(e)
+            log.error("FATAL: unrecognized op '%s'" % nameop['op'])
+            os.abort()
+
+        if opcode in OPCODE_PREORDER_OPS:
+            # preorder
+            op_seq = self.commit_state_preorder(nameop, current_block_number)
+            op_seq_type_str = "state_preorder"
+
+        elif opcode in OPCODE_CREATION_OPS:
+            # creation
+            history_id_key = state_create_get_history_id_key(nameop)
+            history_id = nameop[history_id_key]
+            op_seq = self.commit_state_create(nameop, current_block_number)
+            op_seq_type_str = "state_create"
+
+        elif opcode in OPCODE_TRANSITION_OPS:
+            # transition
+            history_id_key = state_transition_get_history_id_key(nameop)
+            history_id = nameop[history_id_key]
+            op_seq = self.commit_state_transition(nameop, current_block_number)
+            op_seq_type_str = "state_transition"
+
+        else:
+            raise Exception("Unknown operation '%s'" % opcode)
+
+        if op_seq is None:
+            log.error("FATAL: no op-sequence generated (for %s)" % op_seq_type_str)
+            os.abort()
+
+        if type(op_seq) != list:
+            op_seq = [op_seq]
+
+        # make sure all the mutate fields necessary to derive
+        # the next consensus hash are in place.
+        for i in xrange(0, len(op_seq)):
+
+            cur = self.db.cursor()
+            history = None
+
+            # temporarily store history...
+            if history_id is not None:
+                history = namedb_get_history(cur, history_id)
+                op_seq[i]['history'] = history
+
+                # set all extra consensus fields
+            self.add_all_commit_consensus_values(opcode, op_seq[i], nameop, current_block_number)
+
+            # revert...
+            if history is not None:
+                del op_seq[i]['history']
+
+            self.log_commit(current_block_number, op_seq[i]['vtxindex'], op_seq[i]['op'], opcode, op_seq[i])
+
+        return op_seq
+
+
+    def commit_finished( self, block_id ):
+        """
+        Called when the block is finished.
+        Commits all data.
+        """
+
+        self.db.commit()
