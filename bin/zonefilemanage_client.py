bf862ae5f95b164bab206e5b8ac2ec99d8550744
wutentcoding <wutengwuteng@outlook.com>
2017-03-21 21:18:27 +0800

    1

--- bin/zonefilemanage_client.py
+++ bin/zonefilemanage_client.py
@@ -11,19 +11,32 @@ def vote_for_name(name, action, block_id, poll):
         vote_for_name_to_one(name, action, block_id, poll, h)
     return True
 
+proxy_pool = {}
+
 def vote_for_name_to_one(name, action, block_id, poll, ip):
     log.info("%s vote for name %s to ip: %s, the poll is %s in block: %s" % (get_my_ip(), name, ip, poll, block_id))
-    s = xmlrpclib.ServerProxy('http://%s:%s' % (ip, RPC_SERVER_PORT))
+
+    s = get_proxy(ip)
+
     s.rpc_vote_for_name_action(name, action, block_id, poll)
     return True
+
+
+def get_proxy(ip):
+    url = 'http://%s:%s' % (ip, RPC_SERVER_PORT)
+    if url not in proxy_pool.keys():
+        s = xmlrpclib.ServerProxy(url)
+        proxy_pool[url] = s
+    else:
+        s = proxy_pool[url]
+    return s
 
 
 def declare_block_owner(block_id, owner_ip):
     hosts = get_other_ips()
     log.info("other hosts is %s" % hosts)
     for h in hosts:
-        s = xmlrpclib.ServerProxy('http://%s:%s' % (h, RPC_SERVER_PORT))
-        log.info("Sending block owner message to %s" % h)
+        s = get_proxy(h)
         s.rpc_declare_block_owner(block_id, owner_ip)
     return True
 
