5616397a41f3a06a042afa01960179d41779723b
wutentcoding <wutengwuteng@outlook.com>
2017-03-22 22:09:45 +0800

    test

--- expriment/test_well_op.py
+++ expriment/test_well_op.py
@@ -1,24 +1,35 @@
 import xmlrpclib
 import time
+import random
 RPC_SERVER_PORT = 16264
 cnt = 0
-s = xmlrpclib.ServerProxy('http://172.17.0.2:%s' % RPC_SERVER_PORT)
-for i in range(0, 10):
-    res = s.rpc_get_name('foo' + str(i))
-    if res != None:
-        cnt += 1
-    print res
-s = xmlrpclib.ServerProxy('http://172.17.0.3:%s' % RPC_SERVER_PORT)
-for i in range(0, 10):
-    res = s.rpc_get_name('bar' + str(i))
-    if res != None:
-        cnt += 1
-    print res
-s = xmlrpclib.ServerProxy('http://172.17.0.4:%s' % RPC_SERVER_PORT)
-for i in range(0, 10):
-    res = s.rpc_get_name('chr' + str(i))
-    print res
-    if res != None:
+node1 = xmlrpclib.ServerProxy('http://172.17.0.2:%s' % RPC_SERVER_PORT)
+node2 = xmlrpclib.ServerProxy('http://172.17.0.3:%s' % RPC_SERVER_PORT)
+node3 = xmlrpclib.ServerProxy('http://172.17.0.4:%s' % RPC_SERVER_PORT)
+
+proxy = [node1, node2, node3]
+
+def generate_name_set():
+    name_list = []
+    for i in range(0, 100):
+        name_list.append('bar' + str(i))
+
+namelist = generate_name_set()
+
+# do register name
+for name in namelist:
+    node_index = random.randint(0, 2)
+    proxy = proxy[node_index]
+    proxy.rpc_register_name(name)
+    time.sleep(2)
+# flush
+node2.rpc_register_name('flush')
+
+# do count successful name register
+for name in namelist:
+    result = node2.rpc_get_name(name)
+    if result is not None:
         cnt += 1
 
-print '**************cnt = %s *************' % cnt
+print '*********************cnt = %s ********************' % cnt
+
