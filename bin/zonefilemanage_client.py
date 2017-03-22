a4ed5c4f985e4e14b1db13b3edb0ab9d23d15db3
wutentcoding <wutengwuteng@outlook.com>
2017-03-22 16:40:37 +0800

    10 nodes

--- bin/zonefilemanage_client.py
+++ bin/zonefilemanage_client.py
@@ -13,14 +13,17 @@ def vote_for_name(name, action, block_id, poll):
 proxy_pool = {}
 
 def vote_for_name_to_one(name, action, block_id, poll, ip):
-    log.info("%s vote for name %s to ip: %s, the poll is %s in block: %s" % (get_my_ip(), name, ip, poll, block_id))
+    try:
+        log.info("%s vote for name %s to ip: %s, the poll is %s in block: %s" % (get_my_ip(), name, ip, poll, block_id))
 
-    # s = get_proxy(ip)
-    url = 'http://%s:%s' % (ip, RPC_SERVER_PORT)
-    s = xmlrpclib.ServerProxy(url)
-    s.rpc_vote_for_name_action(name, action, block_id, poll)
-    s('close')
-    return True
+        # s = get_proxy(ip)
+        url = 'http://%s:%s' % (ip, RPC_SERVER_PORT)
+        s = xmlrpclib.ServerProxy(url)
+        s.rpc_vote_for_name_action(name, action, block_id, poll)
+        s('close')
+        return True
+    except Exception, e:
+        log.exception("Vote error", e)
 
 
 
@@ -36,15 +39,18 @@ def get_proxy(ip):
 
 
 def declare_block_owner(block_id, owner_ip):
-    hosts = get_other_ips()
-    log.info("other hosts is %s" % hosts)
-    for h in hosts:
-        # s = get_proxy(h)
-        url = 'http://%s:%s' % (h, RPC_SERVER_PORT)
-        s = xmlrpclib.ServerProxy(url)
-        s.rpc_declare_block_owner(block_id, owner_ip)
-        s('close')
-    return True
+    try:
+        hosts = get_other_ips()
+        log.info("other hosts is %s" % hosts)
+        for h in hosts:
+            # s = get_proxy(h)
+            url = 'http://%s:%s' % (h, RPC_SERVER_PORT)
+            s = xmlrpclib.ServerProxy(url)
+            s.rpc_declare_block_owner(block_id, owner_ip)
+            s('close')
+        return True
+    except Exception, e:
+        log.exception("Declare block error", e)
 
 
 def get_name_action_status(name, action):
