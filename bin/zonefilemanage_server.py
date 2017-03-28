230768b1432a4da86d50b54fd64011fcf0727fd0
wutentcoding <wutengwuteng@outlook.com>
2017-03-28 16:22:56 +0800

    with prob

--- bin/zonefilemanage_server.py
+++ bin/zonefilemanage_server.py
@@ -600,20 +600,19 @@ class ZonefileManageRPC(SimpleXMLRPCServer):
         if name_action_blockid not in self.vote_poll.keys():
             return False
 
-        # num = random.randint(1, 10)
-        # if num > 1:
-        #     self.vote_count[name_action_blockid] += 1
+        num = random.randint(1, 10)
+        if num > 1:
+            self.vote_poll[name_action_blockid] += 1
 
-        # For false register
-        # self.vote_count[name_action_blockid] += 1
 
         # For true register
-        # if is_main_worker():
-        #     return False
+
+        if is_main_worker():
+            return False
 
         # For false register
-        if is_main_worker():
-            return True
+        # if is_main_worker():
+        #     return True
 
         self.vote_count[name_action_blockid] += 1
         try:
